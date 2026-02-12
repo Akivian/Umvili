d6ecd83f4845b5a26a363c6cb37e138fc3d62c3d
Akivian <19802992829@163.com>
2026-02-12 23:30:11 +0800

    fix error of paths

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/Umvili' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Umvili/' : '',
   trailingSlash: true,
   images: { unoptimized: true },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 module.exports = nextConfig;
